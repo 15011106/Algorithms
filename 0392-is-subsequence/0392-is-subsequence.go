bf42cfb2d4c1551c1508ff5ad7dab055311e951a
Kwon Min hyeok <rnjsals1575@naver.com>
2024-03-14 21:21:07 +0900

    Time: 1 ms (83.1%), Space: 2 MB (27.22%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.go
@@ -0,0 +1,22 @@
+func isSubsequence(s string, t string) bool {
+
+    pointerOne := 0
+    pointerTwo := 0
+
+    for pointerOne < len(s) && pointerTwo < len(t){
+        if s[pointerOne] == t[pointerTwo]{
+            pointerOne++
+            pointerTwo++
+        } else{
+            pointerTwo++
+        }
+    }
+
+
+        if pointerOne == len(s){
+            return true
+        }else{
+            return false
+        }
+
+}
