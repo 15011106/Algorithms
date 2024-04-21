3f248647d1410297c9a5063e9c357d642e799861
Kwon Min hyeok <rnjsals1575@naver.com>
2024-04-21 23:45:16 +0900

    Time: 4 ms (36.3%), Space: 2.6 MB (25.59%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.go
@@ -0,0 +1,13 @@
+func reverseBits(num uint32) uint32 {
+      var result uint32
+
+    for i := 0; i < 32; i++ {
+        bit := num & 1
+
+        result = (result << 1) | bit
+
+        num >>= 1
+    }
+
+    return result
+}
