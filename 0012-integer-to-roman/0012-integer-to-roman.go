555839ceb5c744a0cfe202f1e02250544b1d5ed7
Kwon Min hyeok <rnjsals1575@naver.com>
2024-05-19 23:57:54 +0900

    Time: 0 ms (100%), Space: 3.3 MB (93.99%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.go
@@ -0,0 +1,31 @@
+func intToRoman(num int) string {
+    	valueSymbols := []struct {
+		value  int
+		symbol string
+	}{
+		{1000, "M"},
+		{900, "CM"},
+		{500, "D"},
+		{400, "CD"},
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
+	}
+
+	var roman strings.Builder
+
+	for _, pair := range valueSymbols {
+		for num >= pair.value {
+			roman.WriteString(pair.symbol)
+			num -= pair.value
+		}
+	}
+
+	return roman.String()
+}
