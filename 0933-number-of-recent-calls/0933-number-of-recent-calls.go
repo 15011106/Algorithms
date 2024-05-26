f98edf0a033f9a6fa3c96a1a8ae075fc5a734257
Kwon Min hyeok <rnjsals1575@naver.com>
2024-05-26 23:45:05 +0900

    Time: 152 ms (21.84%), Space: 12.4 MB (5.01%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.go
@@ -0,0 +1,17 @@
+
+type RecentCounter struct {
+    requests []int
+}
+
+func Constructor() RecentCounter {
+    return RecentCounter{requests: make([]int, 0)}
+}
+
+func (this *RecentCounter) Ping(t int) int {
+    this.requests = append(this.requests, t)
+
+    for len(this.requests) > 0 && this.requests[0] < t-3000 {
+        this.requests = this.requests[1:]
+    }
+    return len(this.requests)
+}
