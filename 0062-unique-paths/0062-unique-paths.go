f1cbc5981c57b9c7678d0a446abd898daea3bf5a
Kwon Min hyeok <rnjsals1575@naver.com>
2025-02-03 09:41:12 +0900

    Time: 0 ms (100%), Space: 4 MB (59.53%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.go
+++ 0062-unique-paths/0062-unique-paths.go
@@ -1,22 +1,22 @@
 func uniquePaths(m int, n int) int {
-    ans := make([][]int, n)
-    for i:=0 ;i<n; i++{
-        ans[i] = make([]int, m)
+    ans := make([][]int, m)
+    for i:=0 ;i<m; i++{
+        ans[i] = make([]int, n)
     }
 
     for i:=0; i<m; i++{
-        ans[0][i] = 1
+        ans[i][0] = 1
     }
 
     for j:=0 ;j<n; j++{
-        ans[j][0] = 1
+        ans[0][j] = 1
     }
     
     for i:=1 ; i < m ; i++{
         for j:=1; j < n; j++{
-            ans[j][i] = ans[j-1][i] + ans[j][i-1]
+            ans[i][j] = ans[i-1][j] + ans[i][j-1]
         }
     }
 
-    return ans[n-1][m-1]
+    return ans[m-1][n-1]
 }
