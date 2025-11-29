2bb953e18c2a4a6891fa7e10290c248b42104c58
Kwon Min hyeok <rnjsals1575@naver.com>
2025-11-29 17:43:38 +0900

    Time: 0 ms (100%), Space: 4.3 MB (60.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.go
+++ 0020-valid-parentheses/0020-valid-parentheses.go
@@ -1,55 +1,63 @@
-type stack []interface{}
-func (s *stack) IsEmpty() bool {
-	if len(*s) == 0 {
-		return true
-	}
-	return false
-}
-
-func (s *stack) Push(data interface{}) {
-	*s = append(*s, data)
-}
-
-func (s *stack) Peek() interface{} {
-	if s.IsEmpty() {
-		fmt.Println("stack is empty")
-		return nil
-	} else {
-		top := len(*s) - 1
-		data := (*s)[top]
-		return data
-	}
-}
-
-func (s *stack) Pop() interface{} {
-	if s.IsEmpty() {
-		fmt.Println("stack is empty")
-		return nil
-	} else {
-		top := len(*s) - 1
-		data := (*s)[top]
-		*s = (*s)[:top]
-		return data
-	}
-}
-
 func isValid(s string) bool {
-	var st stack
-	brackets := map[rune]rune{
-		'(': ')',
-		'[': ']',
-		'{': '}',
-	}
 
-	for _, char := range s {
-		if _, isOpening := brackets[char]; isOpening {
-			st.Push(char)
-		} else if !st.IsEmpty() && brackets[st.Peek().(rune)] == char {
-			st.Pop()
-		} else {
-			return false
-		}
-	}
+    if (len(s) %2 != 0){
+        return false
+    }
 
-	return st.IsEmpty()
+    var stack Stack[rune]
+    pairs := make(map[rune]rune)
+    pairs[')'] = '('
+    pairs['}'] = '{'
+    pairs[']'] = '['
+
+
+    for _,v := range s{
+        switch (v){
+            case '(', '{', '[':
+            stack.push(v)
+            
+            case ')', '}', ']':
+            if stack.isEmpty(){
+                return false
+            }
+
+            pop, t := stack.pop()
+            if t == false {
+                return false
+            }
+
+            if pop != pairs[v]{
+                return false
+            }
+            default:
+        }
+    }
+
+    return stack.isEmpty()
+}
+
+type Stack[T any] struct{
+    data []T
+}
+
+func (s *Stack[T]) push(x T){
+    s.data = append(s.data, x)
+}
+
+func (s *Stack[T]) pop() (T, bool){
+    var zero T
+    if len(s.data) == 0 {
+        return zero, false
+        }
+    top := s.data[len(s.data)-1]
+    s.data = s.data[:len(s.data)-1]
+    return top, true
+}
+
+func (s *Stack[T]) isEmpty () bool{
+    if len(s.data) == 0{
+        return true
+    }
+
+    return false
 }
