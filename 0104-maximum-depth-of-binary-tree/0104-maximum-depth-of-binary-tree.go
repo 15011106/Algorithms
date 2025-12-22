1178233afc735f7278e304e61a1e9cc48a4e8d9f
Kwon Min hyeok <rnjsals1575@naver.com>
2025-12-22 16:59:25 +0900

    Time: 0 ms (100%), Space: 6.4 MB (14.92%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.go
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.go
@@ -1,8 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 func maxDepth(root *TreeNode) int {
-	if root == nil {
-		return 0
-	}
-	leftDepth := maxDepth(root.Left)
-	rightDepth := maxDepth(root.Right)
-	return 1 + int(math.Max(float64(leftDepth), float64(rightDepth)))
+    max := 0
+
+    var dfs func(node *TreeNode, level int)
+
+    dfs = func(node *TreeNode, level int){
+
+        if node == nil{
+            if level > max {
+                max = level
+                }
+            return
+        }
+
+        dfs(node.Left, level+1)
+        dfs(node.Right, level+1)
+    }
+    
+    dfs(root, 0)
+    return max
 }
