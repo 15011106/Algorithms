ba216703e47de8d55aa53303c89ec6525e77a5ac
Kwon Min hyeok <rnjsals1575@naver.com>
2025-12-23 17:01:11 +0900

    Time: 0 ms (100%), Space: 7.4 MB (13.53%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.go
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.go
@@ -8,32 +8,40 @@
  */
 func isBalanced(root *TreeNode) bool {
 
-    ans, _ := dfs(root)
+    var dfs func(balanced bool, root *TreeNode) (bool, int)
+    dfs = func(balanced bool, node *TreeNode)(ans bool, depth int) {
+
+        if !balanced {
+            return false, 0
+        }
+
+        if node == nil {
+            return true, 0
+        }
+
+        lb, left := dfs(balanced, node.Left)
+        rb, right := dfs(balanced, node.Right)
+
+        if !lb {
+            return false, 0
+        }
+
+        if !rb{
+            return false, 0
+        }
+
+        if !(right - left == 1 || left - right == 1 || right - left == 0 || left-right ==0) {  
+            return false , depth
+        }
+        
+        if left > right{
+            return true, left + 1
+        }
+
+        return true, right + 1
+    }
+
+    ans, _ := dfs(true, root)
+
     return ans
-}
-
-func dfs (node *TreeNode) (bool, int){
-
-    if node == nil{
-        return true, 0
-    }
-    
-    isBalanceLeft, left := dfs(node.Left)
-    isBalanceRight, right := dfs(node.Right)
-
-    diff := left - right
-    if diff < 0 {
-        diff = -diff
-    }
-
-    max := left 
-    if left < right{
-        max = right
-    }
-
-    if isBalanceLeft && isBalanceRight && diff <= 1{
-        return true, max+1
-    }
-
-    return false, -1
 }
