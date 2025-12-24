e7775413b3275b71a5e7a1ff862ffda77f470c27
Kwon Min hyeok <rnjsals1575@naver.com>
2025-12-24 11:34:53 +0900

    Time: 0 ms (100%), Space: 4.5 MB (0.27%) - LeetHub

--- 0100-same-tree/0100-same-tree.go
+++ 0100-same-tree/0100-same-tree.go
@@ -8,11 +8,35 @@
  */
 func isSameTree(p *TreeNode, q *TreeNode) bool {
 
-    if p == nil && q == nil {
-        return true
-    }
-    if p == nil || q == nil {
+    arr1 := dfs(p)
+    arr2 := dfs(q)
+
+    if len(arr1) != len(arr2){
         return false
     }
-    return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+
+    for i :=0; i<len(arr1) ;i++{
+        if arr1[i] != arr2[i]{
+            return false
+        }
+    }
+
+    return true
+}
+
+func dfs(curNode *TreeNode) []string{
+    if curNode == nil{
+        return []string{"null"}
+    }
+
+    
+    arr := []string{strconv.Itoa(curNode.Val)}
+
+    left := dfs(curNode.Left)
+    arr = append(arr,left...)
+    right := dfs(curNode.Right)
+    arr = append(arr,right...)
+    
+    return arr
+
 }
