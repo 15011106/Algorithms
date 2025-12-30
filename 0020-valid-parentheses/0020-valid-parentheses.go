cdec8ad64a221ab1e70802915f9ba033bebd485f
Kwon Min hyeok <rnjsals1575@naver.com>
2025-12-30 14:21:45 +0900

    Time: 0 ms (100%), Space: 4.3 MB (62.01%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.go
+++ 0020-valid-parentheses/0020-valid-parentheses.go
@@ -1,60 +1,62 @@
 func isValid(s string) bool {
 
-    if (len(s) %2 != 0){
+    var stack Stack[rune]
+
+    for _, v := range s{
+
+        switch (v){
+             case '(', '{', '[':
+                stack.push(v)
+            
+            case ')', '}', ']':
+                currentChar := stack.pop()
+                if v == ')'{
+                    if currentChar != '('{
+                        return false
+                    }
+                }
+                if v == '}'{
+                    if currentChar !='{'{
+                        return false
+                    }
+                }
+                if v == ']'{
+                    if currentChar != '['{
+                        return false
+                    }
+                }
+            }
+        }
+
+    if !stack.isEmpty(){
         return false
     }
 
-    var stack Stack[rune]
-    pairs := make(map[rune]rune)
-    pairs[')'] = '('
-    pairs['}'] = '{'
-    pairs[']'] = '['
-
-
-    for _,v := range s{
-        switch (v){
-            case '(', '{', '[':
-            stack.push(v)
-            
-            case ')', '}', ']':
-            if stack.isEmpty(){
-                return false
-            }
-
-            pop, t := stack.pop()
-            if t == false {
-                return false
-            }
-
-            if pop != pairs[v]{
-                return false
-            }
-            default:
-        }
-    }
-
-    return stack.isEmpty()
+    return true
 }
 
-type Stack[T any] struct{
+type Stack[T any] struct {
     data []T
 }
 
-func (s *Stack[T]) push(x T){
+func (s *Stack[T])push(x T){
     s.data = append(s.data, x)
 }
 
-func (s *Stack[T]) pop() (T, bool){
-    var zero T
-    if len(s.data) == 0 {
-        return zero, false
-        }
-    top := s.data[len(s.data)-1]
+func (s *Stack[T]) pop() (x T){
+    
+    if s.isEmpty(){
+        return x
+    }
+
+    popped := s.data[len(s.data)-1]
     s.data = s.data[:len(s.data)-1]
-    return top, true
+
+    return popped
 }
 
-func (s *Stack[T]) isEmpty () bool{
+func (s *Stack[T]) isEmpty() bool{
+
     if len(s.data) == 0{
         return true
     }
