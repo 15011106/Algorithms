505c76c4b2cd0a22f6e0617b8f6e6f23757542eb
Kwon Min hyeok <rnjsals1575@naver.com>
2026-01-14 04:42:19 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.go
+++ 0057-insert-interval/0057-insert-interval.go
@@ -1,58 +1,18 @@
 func insert(intervals [][]int, newInterval []int) [][]int {
-    ans := [][]int{}
-    startNum, endNum := 0, 0
-    flag := false
-
-    if len(intervals) == 0 {
-        return append(ans, newInterval)
+   for i, seg := range intervals {
+    if seg[1] < cur[0] {
+        res = append(res, seg)
+        continue
     }
 
-    for i := 0; i < len(intervals); i++ {
-        s := intervals[i][0]
-        e := intervals[i][1]
-
-        if !flag && s > newInterval[1] {
-            ans = append(ans, newInterval)
-            ans = append(ans, intervals[i:]...)
-            return ans
-        }
-
-        if flag && s > endNum {
-            ans = append(ans, []int{startNum, endNum})
-            ans = append(ans, intervals[i:]...)
-            return ans
-        }
-
-        if !flag && e < newInterval[0] {
-            ans = append(ans, intervals[i])
-            continue
-        }
-
-        if !flag {
-            flag = true
-            if s < newInterval[0] {
-                startNum = s
-            } else {
-                startNum = newInterval[0]
-            }
-
-            if e > newInterval[1] {
-                endNum = e
-            } else {
-                endNum = newInterval[1]
-            }
-        } else {
-            if e > endNum {
-                endNum = e
-            }
-        }
+    if seg[0] > cur[1] {
+        res = append(res, cur)        
+        res = append(res, intervals[i:]...)
+        return res
     }
 
-    if !flag {
-        ans = append(ans, newInterval)
-        return ans
-    }
-
-    ans = append(ans, []int{startNum, endNum})
-    return ans
-    }
+    if seg[0] < cur[0] { cur[0] = seg[0] }
+    if seg[1] > cur[1] { cur[1] = seg[1] }
+}
+res = append(res, cur)
+return res}
