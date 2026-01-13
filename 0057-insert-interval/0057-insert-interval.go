8bd3fcec98acd7f73eb22959d284f70fd9dab771
Kwon Min hyeok <rnjsals1575@naver.com>
2026-01-14 04:42:44 +0900

    Time: 0 ms (100%), Space: 6.2 MB (14.14%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.go
+++ 0057-insert-interval/0057-insert-interval.go
@@ -1,18 +1,27 @@
 func insert(intervals [][]int, newInterval []int) [][]int {
-   for i, seg := range intervals {
-    if seg[1] < cur[0] {
-        res = append(res, seg)
-        continue
+    res := [][]int{}
+    cur := []int{newInterval[0], newInterval[1]}
+
+    for i, seg := range intervals {
+        if seg[1] < cur[0] {
+            res = append(res, seg)
+            continue
+        }
+
+        if seg[0] > cur[1] {
+            res = append(res, cur)
+            res = append(res, intervals[i:]...)
+            return res
+        }
+
+        if seg[0] < cur[0] {
+            cur[0] = seg[0]
+        }
+        if seg[1] > cur[1] {
+            cur[1] = seg[1]
+        }
     }
 
-    if seg[0] > cur[1] {
-        res = append(res, cur)        
-        res = append(res, intervals[i:]...)
-        return res
-    }
-
-    if seg[0] < cur[0] { cur[0] = seg[0] }
-    if seg[1] > cur[1] { cur[1] = seg[1] }
+    res = append(res, cur)
+    return res
 }
-res = append(res, cur)
-return res}
