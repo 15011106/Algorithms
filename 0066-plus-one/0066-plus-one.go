2235ac7b20c96f669b85625c89b1e246bab1526d
Kwon Min hyeok <rnjsals1575@naver.com>
2026-01-20 14:25:20 +0900

    Time: 0 ms (100%), Space: 4 MB (31.57%) - LeetHub

--- 0066-plus-one/0066-plus-one.go
+++ 0066-plus-one/0066-plus-one.go
@@ -1,12 +1,31 @@
 func plusOne(digits []int) []int {
-     for i := len(digits) - 1; i >= 0; i-- {
-        digits[i]++
-        if digits[i] == 10 {
+    
+    carry := false
+    for i:=len(digits)-1; i>=0; i--{
+        
+        if carry {
+            if digits[i]+1 > 9{
+                digits[i] = 0
+            }else{
+                digits[i]++
+                return digits
+            }
+        }else{
+
+        if digits[i] + 1 > 9{
+            carry = true
             digits[i] = 0
-        } else {
+        }else{
+            digits[i]++
             return digits
         }
+        }
     }
-    
-    return append([]int{1}, digits...)
+
+    ans := make([]int, len(digits)+1)
+    if carry{
+        ans[0] = 1
+    }
+
+    return ans
 }
