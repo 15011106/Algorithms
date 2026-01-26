29e1e5e16efd636d564500422e37bbab63887a58
Kwon Min hyeok <rnjsals1575@naver.com>
2026-01-26 18:20:30 +0900

    Time: 77 ms (64.58%), Space: 13.9 MB (16.41%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.go
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func goodNodes(root *TreeNode) int {
+
+    ans := 0
+    var dfs func(node *TreeNode, max int)
+    dfs = func(node *TreeNode, max int){
+
+        if node == nil{
+           return
+        }
+
+        if node.Val >= max{
+            ans++
+            max = node.Val
+        }
+        
+        dfs(node.Left, max)
+        dfs(node.Right, max)
+        
+    }
+
+    dfs(root,root.Val)
+
+    return ans
+}
