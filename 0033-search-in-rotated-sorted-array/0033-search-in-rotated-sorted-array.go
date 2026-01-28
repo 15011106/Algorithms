063eb06aae9045861af816fe123d94017488b4ef
Kwon Min hyeok <rnjsals1575@naver.com>
2026-01-28 11:57:38 +0900

    Time: 0 ms (100%), Space: 4.5 MB (36.17%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
@@ -1,37 +1,28 @@
 func search(nums []int, target int) int {
 
-    low := 0
-    high := len(nums)-1
+    l := 0
+    h := len(nums)-1
 
-    for (low < high){
-        mid := (low + high)/2
-        if nums[mid] > nums[high]{
-            low = mid + 1
-        }else{
-            high = mid
-        }
-    }
-
-   pivot := low
-    low = 0
-    high = len(nums) - 1
-
-    if target >= nums[pivot] && target <= nums[high] {
-        low = pivot
-    } else {
-        high = pivot - 1
-    }
-
-    for low <= high {
-        mid := (low + high) / 2
-        if nums[mid] == target {
+    for l <= h{
+        mid := (l+h)/2
+        if nums[mid] == target{
             return mid
-        } else if nums[mid] < target {
-            low = mid + 1
-        } else {
-            high = mid - 1
+        }
+
+        if nums[l] <= nums[mid]{
+            if nums[l] <= target && target < nums[mid]{
+                h = mid-1
+            }else{
+                l = mid+1
+            }
+        }else{
+            if target > nums[mid] && nums[h] >= target{
+                l= mid +1 
+            }else{
+                h=mid -1
+            }
         }
     }
 
-    return -1 
+    return -1
 }
