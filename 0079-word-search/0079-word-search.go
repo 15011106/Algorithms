d143a9a5d93f0108ad6d39d59e545e87b10b5bc2
Minhyeok Kwon <rnjsals1575@naver.com>
2026-05-12 15:14:14 +0900

    Time: 150 ms (32.44%), Space: 4.2 MB (23.26%) - LeetHub

--- 0079-word-search/0079-word-search.go
+++ 0079-word-search/0079-word-search.go
@@ -1,37 +1,49 @@
+
+
 func exist(board [][]byte, word string) bool {
+
 	visited := make([][]bool, len(board))
-	for i := range visited {
+	for i:= range visited{
 		visited[i] = make([]bool, len(board[0]))
 	}
 
-	ans := false
-	for i := 0; i < len(board); i++ {
-		for j := 0; j < len(board[i]); j++ {
-			if board[i][j] == word[0] {
-				if dfs(i, j, word, board, visited, 0) {
+	for i:=0; i<len(board) ;i++{
+		for j:=0; j<len(board[0]);j++{
+			if !visited[i][j] && board[i][j] == word[0]{
+                visited[i][j] = true
+				if retrieve(board, j, i, 1, word, visited){
 					return true
 				}
+                visited[i][j] = false
 			}
 		}
 	}
-	
-	return ans
+	return false
 }
 
-func dfs(x int, y int, word string, board [][]byte, visited [][]bool, idx int) (ans bool) {
-	if x < 0 || y < 0 || x >= len(board) || y >= len(board[0]) || visited[x][y] || board[x][y] != word[idx] {
+	func retrieve(board [][]byte,curX,curY, idx int, word string, visited [][]bool) bool{
+		if idx == len(word){
+		return true
+	}
+
+		direction := [][]int{{0,1},{0,-1},{1,0},{-1,0}}
+	
+		for i:=0; i<4; i++{
+		x := curX + direction[i][1]
+		y := curY + direction[i][0]
+
+		if x>=0 && x<len(board[0]) && y>=0 && y < len(board){
+		if !visited[y][x] && board[y][x] == word[idx]{
+			visited[y][x] = true
+			if retrieve(board, x, y, idx+1, word, visited){
+                return true
+                }
+                visited[y][x] = false
+            }
+            }    
+        }
+
 		return false
 	}
 
-	if idx == len(word)-1 {
-		return true
-	}
 
-	visited[x][y] = true
-	if dfs(x+1, y, word, board, visited, idx+1) || dfs(x-1, y, word, board, visited, idx+1) || dfs(x, y+1, word, board, visited, idx+1) || dfs(x, y-1, word, board, visited, idx+1) {
-		return true
-	}
-
-	visited[x][y] = false
-	return false
-}
